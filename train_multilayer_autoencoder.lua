ba9745fe4a88df7a59d00c72e9307e1a3963d06d
Alberto Sabater <asabater94@gmail.com>
2016-05-19 10:58:24 +0200

    New options added. Normalize data option

--- train_multilayer_autoencoder.lua
+++ train_multilayer_autoencoder.lua
@@ -6,6 +6,7 @@ require 'nngraph'
 require 'Rectifier'
 require 'cutorch'
 require 'cunn'
+require 'unsup'
 
 
 local ninput = 4
@@ -18,15 +19,56 @@ params = {
     game = "ms_pacman",
     dataFile = "../stored_frames/frames_ms_pacman_0419.t7",
     filename = "ms_pacman" .. "_encoder" .. "_" .. os.date("%m%d"),
-    maxIter = 75,
     learning_rate = 0.01,
-    epochs = 2000,
-    numLayers = 2,
-    feautureRelation = 2
+    maxIter = 40,
+    epochs = 20,
+    numLayers = 1,
+    feautureRelation = 2,
+    normalizeData = 0,
+    gpu = 1,
+    convNet = 'convnet_paper1_bigger',
+    midLayer = 'none',
+    dropout = 0
 }
 
+cmd = torch.CmdLine()
+cmd:option('-game', params.game, 'game')
+cmd:option('-dataFile', params.dataFile, 'path to data')
+cmd:option('-filename', params.filename, 'destination path')
+cmd:option('-learning_rate', params.learning_rate, 'learning rate')
+cmd:option('-maxIter', params.maxIter, 'number of max iterations')
+cmd:option('-epochs', params.epochs, 'epochs')
+cmd:option('-numLayers', params.numLayers, 'number of layers to train')
+cmd:option('-feautureRelation', params.feautureRelation, 'relation to the defined netwokr architecture')
+cmd:option('-normalizeData', params.normalizeData, 'true to normalize data')
+cmd:option('-gpu', params.gpu, '-1 to use CPU')
+cmd:option('-convNet', params.convNet, 'network architecture')
+cmd:option('-midLayer', params.midLayer, 'network architecture')
+cmd:option('-dropout', params.dropout, 'add a dropout layer before decoder')
+options = cmd:parse(arg)
 
-cnv = require 'convnet_paper1_bigger'
+options.filename = "ms_pacman" .. "_encoder"
+if options.normalizeData == 1 then
+    options.filename = options.filename .. "_norm"
+end
+if options.dropout == 1 then
+    options.filename = options.filename .. "_drop"
+elseif options.dropout == 2 then
+    options.filename = options.filename .. "_sDrop"
+end
+options.filename = options.filename .. "_" .. options.numLayers .. "_" .. options.midLayer .. "_" .. os.date("%m%d")
+
+print (options.filename)
+
+for k, v in pairs(options) do
+    print(k, v)
+end
+
+print ("================================================================")
+
+
+local msg, cnv = pcall(require, options.convNet)
+--cnv = require arg.convNet
 args = {}
 args.hist_len = 4
 args.ncols = 1
@@ -42,7 +84,7 @@ decoder = nn.Sequential()
 
 
 
-for i=1,math.min(params.numLayers,#args.n_units) do
+for i=1,math.min(options.numLayers,#args.n_units) do
     if i == 1 then  -- First convolutional layer
       encoder:add(nn.SpatialConvolution(args.hist_len*args.ncols, args.n_units[i]/2,
                           args.filter_size[i], args.filter_size[i],
@@ -70,41 +112,92 @@ end
 
 
 net:add(encoder)
+
+if options.midLayer == "sigmoid" then
+    net:add(nn.Sigmoid())
+elseif options.midLayer == "tanh" then
+    net:add (nn.Tanh())
+elseif options.midLayer == "diag" then
+    outputFeatures = encoder:get(encoder:size()-1).nOutputPlane
+    net:add(nn.Diag(outputFeatures))
+end
+
+if options.dropout == 1 then
+    net:add(nn.Dropout())
+elseif options.dropout == 2 then
+    net:add(nn.SpatialDropout())
+end
 net:add(decoder)
 
-net:cuda()
+if options.gpu >= 0 then
+    net:cuda()
+else
+    net:double()
+end
 
 print (net)
 
---[[
-data = torch.rand(1,4,84,84):cuda()
-print (data:size())
-print(net:forward(data):size())
-]]
+data = torch.load(options.dataFile).frames
+if options.gpu >= 0 then
+    data = data:cuda()
+else
+    data = data:double()
+end
 
 
+if options.normalizeData == 1 then
+    local mean = {}
+    local std = {}
+    for i=1,data:size(1) do
+         mean[i] = data[i]:mean()
+         std[i] = data[i]:std()
+         data[i]:add(-mean[i])
+         data[i]:div(std[i])
+    end
+print ("Data normalized")
 
-data = torch.load(params.dataFile).frames:cuda()
+--[[ Check if datais properly normalized
+    for i=1,data:size(1) do
+         local testMean = data[i]:mean()
+         local testStd = data[i]:std()
+
+         print('       test data, mean:                   ' .. testMean)
+         print('       test data, standard deviation:     ' .. testStd)
+    end
+  ]]
+end
 
 dataset={};
 function dataset:size() return data:size(1) end -- 100 examples
 for i=1,dataset:size() do
-    dataset[i] = {data[1], data[1]}
+    dataset[i] = {data[i], data[i]}
 end
 
 
-criterion = nn.MSECriterion():cuda()
-trainer = nn.StochasticGradient(net, criterion)
-
-trainer.learningRate = params.learning_rate
-trainer.maxIteration = params.maxIter
-
-
-for t=1, params.epochs do
-  print('Epoch ' .. t, "\t", os.date("%x %X"))
-
-  trainer:train(dataset)
-  torch.save("../stored_kernels/autoencoders/" .. params.filename .. ".t7", { network = net:get(1), model = net } )
-
-  print ("Network saved", params.filename, "\t", os.date("%x %X"))
+criterion = nn.MSECriterion()
+if options.gpu >= 0 then
+    criterion:cuda()
+else
+    criterion:double()
 end
+
+--local msg, err = pcall(require, 'CustomStochasticGradient', net, criterion)
+--print (msg, err)
+
+trainer = require "CustomStochasticGradient"
+trainer:init(net, criterion)
+--trainer.__init(net, criterion)
+
+trainer.learningRate = options.learning_rate
+trainer.maxIteration = options.maxIter
+
+
+nClock = os.clock()
+
+for t=1, options.epochs do
+  currenError = trainer:train(dataset)
+end
+
+net:evaluate()
+print ("Elapsed time is: ", os.clock()-nClock)
+torch.save("../stored_kernels/autoencoders/" .. options.filename .. ".t7", { network = net:get(1), model = net } )
